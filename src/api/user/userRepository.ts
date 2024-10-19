f88fd5fb0dbd5bdb6116c2397db05540e55be2e7
Azhar <azharadityapratama48@gmail.com>
2024-10-19 13:08:47 +0700

    fix(users): change from inmemory to prisma

--- src/api/user/userRepository.ts
+++ src/api/user/userRepository.ts
@@ -1,30 +1,34 @@
+import { PrismaClient, User as PrismaUser } from '@prisma/client';
 import type { User } from "@/api/user/userModel";
 
-export const users: User[] = [
-  {
-    id: 1,
-    name: "Alice",
-    email: "alice@example.com",
-    age: 42,
-    createdAt: new Date(),
-    updatedAt: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000), // 5 days later
-  },
-  {
-    id: 2,
-    name: "Robert",
-    email: "Robert@example.com",
-    age: 21,
-    createdAt: new Date(),
-    updatedAt: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000), // 5 days later
-  },
-];
+const prisma = new PrismaClient();
 
 export class UserRepository {
   async findAllAsync(): Promise<User[]> {
-    return users;
+    try {
+      const users: PrismaUser[] = await prisma.user.findMany();
+      return users;
+    } catch (error: unknown) {
+      let errorMessage = 'Error fetching users';
+      if (error instanceof Error) {
+        errorMessage = `${errorMessage}: ${error.message}`;
+      }
+      throw new Error(errorMessage);
+    }
   }
 
-  async findByIdAsync(id: number): Promise<User | null> {
-    return users.find((user) => user.id === id) || null;
+  async findByIdAsync(id: string): Promise<User | null> {
+    try {
+      const user: PrismaUser | null = await prisma.user.findUnique({
+        where: { id },
+      });
+      return user;
+    } catch (error: unknown) {
+      let errorMessage = `Error fetching user with ID ${id}`;
+      if (error instanceof Error) {
+        errorMessage = `${errorMessage}: ${error.message}`;
+      }
+      throw new Error(errorMessage);
+    }
   }
 }
