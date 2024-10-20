02a73bb2920eb42c63b8d57cb6df12bc12beea52
Azhar <azharadityapratama48@gmail.com>
2024-10-21 00:46:11 +0700

    feat: get all lecturers

--- src/api/lecturer/lecturerRepository.ts
+++ src/api/lecturer/lecturerRepository.ts
@@ -1,10 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import type { Lecturer } from "./lecturerModel";
 
 const prisma = new PrismaClient();
 
 export class LecturerRepository {
-  // Method untuk menemukan dosen berdasarkan ID
-  async findById(id: string) {
+  async findAll(): Promise<Lecturer[]> {
+    return await prisma.lecturer.findMany({
+      select: {
+        id: true,
+        name: true,
+        faculty: true,
+        department: true,
+        upvotes: true,
+        downvotes: true,
+      },
+    });
+  }
+
+  async findById(id: string): Promise<Lecturer | null> {
     return await prisma.lecturer.findUnique({
       where: { id },
       select: {
