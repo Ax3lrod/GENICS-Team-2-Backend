3b9bab12ffd5452e70b0c491a1202d09475e1429
AthallaITS <5027231018@student.its.ac.id>
2024-10-31 20:05:36 +0700

    fix: disable env validation

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
-import { cleanEnv, host, num, port, str, testOnly } from "envalid";
 
 dotenv.config();
 
-export const env = cleanEnv(process.env, {
-  NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
-  HOST: host({ devDefault: testOnly("localhost") }),
-  PORT: port({ devDefault: testOnly(3000) }),
-  CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:3000") }),
-  COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
-  COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
-});
+export const env = {
+  NODE_ENV: process.env.NODE_ENV ?? "development",
+  HOST: process.env.HOST ?? "localhost",
+  PORT: process.env.PORT ?? 3000,
+  CORS_ORIGIN: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+  COMMON_RATE_LIMIT_MAX_REQUESTS: process.env.COMMON_RATE_LIMIT_MAX_REQUESTS ?? 1000,
+  COMMON_RATE_LIMIT_WINDOW_MS: process.env.COMMON_RATE_LIMIT_WINDOW_MS ?? 1000,
+  JWT_SECRET: process.env.JWT_SECRET ?? "genicsgenics",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ?? "24h",
+};
