ff70eb8c641eddabc453d7b1e5e29076331b44a1
Dedy Irama <dedyirama.ars@gmail.com>
2024-11-09 09:19:49 +0700

    fix: error on CI test

--- src/common/__tests__/errorHandler.test.ts
+++ src/common/__tests__/errorHandler.test.ts
@@ -8,6 +8,8 @@ describe("Error Handler Middleware", () => {
   let app: Express;
 
   beforeAll(() => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+
     app = express();
 
     app.get("/error", () => {
@@ -18,8 +20,14 @@ describe("Error Handler Middleware", () => {
       next(error);
     });
 
-    app.use(errorHandler());
     app.use("*", (req, res) => res.status(StatusCodes.NOT_FOUND).send("Not Found"));
+    app.use(errorHandler());
+  });
+
+  afterAll(() => {
+    server.close();
+
+    jest.restoreAllMocks();
   });
 
   describe("Handling unknown routes", () => {
